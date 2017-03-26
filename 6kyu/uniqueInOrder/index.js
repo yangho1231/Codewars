18d953a84a2b657c16c56eb0020200cf96932643
yangho1231 <yangho1231@gmail.com>
2017-03-25 18:08:53 -0600

    Added kyu

---
+++ 6kyu/uniqueInOrder/index.js
@@ -0,0 +1,9 @@
+var uniqueInOrder=function(iterable){
+  var arr = [];
+  for(var i = 0; i < iterable.length; i++) {
+    if(iterable[i] !== iterable[i+1]) {
+      arr.push(iterable[i]);
+    }
+  }
+  return arr;
+}

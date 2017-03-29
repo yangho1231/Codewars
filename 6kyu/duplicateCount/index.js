be7ad42328f6d4990220fc23adbb4d21b3307a4c
yangho1231 <yangho1231@gmail.com>
2017-03-29 04:31:24 -0600

    Got different solution

--- 6kyu/duplicateCount/index.js
+++ 6kyu/duplicateCount/index.js
@@ -1,4 +1,17 @@
 function duplicateCount(text){
+  text = text.toLowerCase().split('');
+  var arr = [];
+  var dup = [];
+  for(var i = 0; i < text.length; i++) {
+    if(arr.indexOf(text[i]) === -1) arr.push(text[i]);
+    else if(arr.indexOf(text[i]) > -1 && dup.indexOf(text[i]) === -1) 
+      dup.push(text[i]);
+  }
+  return !text ? 0 : dup.length;
+}
+duplicateCount("");
+//Shorter Solution
+function duplicateCount(text){
   return text.toLowerCase().split('').filter(function(val, i, arr){
     return arr.indexOf(val) !== i && arr.lastIndexOf(val) === i;
   }).length;

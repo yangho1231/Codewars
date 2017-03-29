1485d27e620ace3bd82df657f11a11aa54cbdb3d
yangho1231 <yangho1231@gmail.com>
2017-03-29 04:29:59 -0600

    Added duplicatCount kyu

---
+++ 6kyu/duplicateCount/index.js
@@ -0,0 +1,6 @@
+function duplicateCount(text){
+  return text.toLowerCase().split('').filter(function(val, i, arr){
+    return arr.indexOf(val) !== i && arr.lastIndexOf(val) === i;
+  }).length;
+}
+duplicateCount("Indivisibilities");

a4d8e259cc75d2efebda8d67360238fea8592214
yangho1231 <yangho1231@gmail.com>
2017-04-09 18:54:28 -0600

    Added alternative solution for middleChar

--- 7kyu/GetMiddleChar/index.js
+++ 7kyu/GetMiddleChar/index.js
@@ -1,5 +1,12 @@
 function getMiddle(s)
 {
-  return s.length % 2 === 0 ? s.charAt(s.length/2 - 1) + s.charAt(s.length/2) : s.charAt(Math.floor(s.length/2))
+  return s.length % 2 ? s.charAt(s.length/2 - 1) + s.charAt(s.length/2) : s.charAt(Math.floor(s.length/2))
+}
+getMiddle("teest")
+
+//Another solution
+function getMiddle(s)
+{
+  return s.subStr(Math.ceil(s.length/2 - 1), s.length % 2 === 0 ? 2 : 1);
 }
 getMiddle("teest")
